0eb608f917183054d8550ece47297174bc924436
sekar-1971 <chandrasekaran.dhandapani1971@gmail.com>
2025-06-23 18:10:57 +0530

    test

--- src/main/kotlin/com/example/testapi/controller/TestController.kt
+++ src/main/kotlin/com/example/testapi/controller/TestController.kt
@@ -14,7 +14,7 @@ class TestController(private val calculatorService: CalculatorService) {
         val result = calculatorService.add(request.a, request.b)
         return AddResponse(result = result)
     }
-//6666
+//6644
     @GetMapping("/testadd")
     fun testAddGet(@RequestParam a: Int, @RequestParam b: Int): AddResponse {
         val result = calculatorService.add(a, b)
